b895486dbb692dc0c92ff74adcd057168078ac3e
Yuusaku Miyazaki <toumin.m7@gmail.com>
2015-07-06 17:06:01 +0900

    add recipe to install mocha

--- my-lamp/recipes/nodejs.rb
+++ my-lamp/recipes/nodejs.rb
@@ -1,4 +1,4 @@
-# パッケージ (Node.js, JSDoc)
+# パッケージ (Node.js)
 %w{nodejs npm}.each do |p|
   package p do
     action :install
@@ -12,4 +12,13 @@ bash 'add_jsdoc' do
     npm install -g jsdoc
   EOC
   not_if 'which jsdoc'
+end
+
+# npmでmochaをインストール
+bash 'add_mocha' do
+  user 'root'
+  code <<-EOC
+    npm install -g mocha
+  EOC
+  not_if 'which mocha'
 end
